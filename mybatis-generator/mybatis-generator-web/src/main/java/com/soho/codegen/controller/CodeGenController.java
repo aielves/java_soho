e05dfaf6538b992e1544a9c9e14fd78eb99cae74
aielves <124010356@qq.com>
2018-07-22 09:01:52 +0800

    1.规范并修改生成器提示

--- mybatis-generator/mybatis-generator-web/src/main/java/com/soho/codegen/controller/CodeGenController.java
+++ mybatis-generator/mybatis-generator-web/src/main/java/com/soho/codegen/controller/CodeGenController.java
@@ -30,13 +30,6 @@ public class CodeGenController {
         return codeGenService.generate(dbMessage);
     }
 
-    /*@ResponseBody
-    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    public Object uploadFile(Integer uploadtype, @RequestParam("file") MultipartFile file) throws BizErrorEx {
-        validUserSession();
-        return codeGenService.uploadFile(uploadtype, file);
-    }*/
-
     @ResponseBody
     @RequestMapping(value = "/getUser", method = RequestMethod.POST)
     public Object generate() throws BizErrorEx {
@@ -109,54 +102,6 @@ public class CodeGenController {
             }
         }
         return null;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/downUploadFile", method = RequestMethod.POST)
-    public Object downUploadFile(HttpServletResponse response, String filePath) throws BizErrorEx {
-        validUserSession();
-        try {
-            File file = new File(filePath);
-            if (file.exists()) {
-                response.setContentType("application/force-download");// 设置强制下载不打开
-                response.addHeader("Content-Disposition",
-                        "attachment;fileName=" + filePath.substring(filePath.lastIndexOf(File.separator)));// 设置文件名
-                byte[] buffer = new byte[1024];
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
-                try {
-                    fis = new FileInputStream(file);
-                    bis = new BufferedInputStream(fis);
-                    OutputStream os = response.getOutputStream();
-                    int i = bis.read(buffer);
-                    while (i != -1) {
-                        os.write(buffer, 0, i);
-                        i = bis.read(buffer);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (fis != null) {
-                        try {
-                            fis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-            return null;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        throw new BizErrorEx(BizErrorCode.BIZ_ERROR, "下载失败");
     }
 
     private void validUserSession() throws BizErrorEx {
